2b6ac41d2076f823a076e68c1218de64aeee6089
rizz <iburykin071@gmail.com>
2025-04-05 19:20:53 +0300

    Yandex Metrika added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+import YandexMetrika from "./components/YandexMetrika/YandexMetrika.jsx";
+
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import "./globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./lib/theme";
 import Navbar from "./components/Navbar/Navbar";
 import FooterContainer from "./components/Footer/FooterContainer";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Пластиковые окна в Туле от производителя!",
@@ -19,6 +23,24 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
+                <Script id="metrika-counter" strategy="afterInteractive">
+                    {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+                    ym(16442995, "init", {
+                            defer: true,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                    });`}
+                </Script>
+                <Suspense fallback={<></>}>
+                    <YandexMetrika />
+                </Suspense>
                 <AppRouterCacheProvider>
                     <ThemeProvider theme={theme}>
                         <header>
