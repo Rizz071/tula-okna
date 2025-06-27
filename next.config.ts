64174d5aacb44a0c35836f032540fba448106183
rizz <iburykin071@gmail.com>
2025-06-27 10:38:22 +0300

    ceny 404 fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/ceny',
+                destination: '/ceny/stoimost-tipovyh-okon',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 export default nextConfig
